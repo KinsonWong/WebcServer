a27200aa02cc0fc76a37f014f54f3568bfaaa56d
JiexiangZheng <sam123love@163.com>
2020-01-05 13:34:21 +0800

    2020.1.5

--- webcserver.c
+++ webcserver.c
@@ -1,6 +1,6 @@
 #include "util.h"
 
-void doit(int fd);
+void request_handler(int fd);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
 void serve_static(int fd, char *filename, int filesize);
@@ -28,12 +28,12 @@ int main(int argc, char **argv)
         Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-	doit(connfd);                                             
+	request_handler(connfd);                                             
 	Close(connfd);                                           
     }
 }
 
-void doit(int fd) 
+void request_handler(int fd) 
 {
     int is_static;
     struct stat sbuf;
@@ -48,7 +48,7 @@ void doit(int fd)
     sscanf(buf, "%s %s %s", method, uri, version);      
     if (strcasecmp(method, "GET")) {                     
         clienterror(fd, method, "501", "Not Implemented",
-                    "Tiny does not implement this method");
+                    "WebcServer does not implement this method  ");
         return;
     }                                                    
     read_requesthdrs(&rio);                              
@@ -57,14 +57,14 @@ void doit(int fd)
     is_static = parse_uri(uri, filename, cgiargs);       
     if (stat(filename, &sbuf) < 0) {                     
 	clienterror(fd, filename, "404", "Not found",
-		    "Tiny couldn't find this file");
+		    "WebcServer couldn't find this file  ");
 	return;
     }                                                   
 
     if (is_static) { /* Serve static content */          
 	if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { 
 	    clienterror(fd, filename, "403", "Forbidden",
-			"Tiny couldn't read the file");
+			"WebcServer couldn't read the file  ");
 	    return;
 	}
 	serve_static(fd, filename, sbuf.st_size);        
@@ -72,7 +72,7 @@ void doit(int fd)
     else { /* Serve dynamic content */
 	if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { 
 	    clienterror(fd, filename, "403", "Forbidden",
-			"Tiny couldn't run the CGI program");
+			"WebcServer couldn't run the CGI program  ");
 	    return;
 	}
 	serve_dynamic(fd, filename, cgiargs);            
@@ -125,7 +125,7 @@ void serve_static(int fd, char *filename, int filesize)
  
     get_filetype(filename, filetype);       
     sprintf(buf, "HTTP/1.0 200 OK\r\n");    
-    sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+    sprintf(buf, "%sServer: WebcServer\r\n", buf);
     sprintf(buf, "%sConnection: close\r\n", buf);
     sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
     sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
@@ -160,7 +160,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
 
     sprintf(buf, "HTTP/1.0 200 OK\r\n"); 
     Rio_writen(fd, buf, strlen(buf));
-    sprintf(buf, "Server: Tiny Web Server\r\n");
+    sprintf(buf, "Server: WebcServer\r\n");
     Rio_writen(fd, buf, strlen(buf));
   
     if (Fork() == 0) { 
@@ -172,16 +172,15 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
     Wait(NULL); 
 }
 
-void clienterror(int fd, char *cause, char *errnum, 
-		 char *shortmsg, char *longmsg) 
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) 
 {
     char buf[MAXLINE], body[MAXBUF];
 
-    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body, "<html><title>Server Error</title>");
     sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
     sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
     sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+    sprintf(body, "%s<hr><em> WebcServer </em>\r\n", body);
 
     sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
     Rio_writen(fd, buf, strlen(buf));
